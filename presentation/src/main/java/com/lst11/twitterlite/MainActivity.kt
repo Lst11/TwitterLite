98fc77a1b9c03780f46645a8a27c533d026dc74d
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-02-08 23:00:05 +0300

    gradle modules - set up configuration

---
+++ presentation/src/main/java/com/lst11/twitterlite/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lst11.twitterlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
