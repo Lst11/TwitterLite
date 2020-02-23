54a4d6fd58e046edece6d5c5f970520ca30aa1ab
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-02-23 16:09:12 +0300

    spock dependencies

--- data/src/test/java/com/lst11/data/ExampleUnitTest.kt
+++ data/src/test/java/com/lst11/data/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.lst11.data
 
+import com.lst11.twitterlite.UserService
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val repository = UserService()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+
+    fun addUserTest() {
+        repository.addUser("Test one")
+        repository.addUser("Test two")
+        repository.addUser("Test three")
     }
 }
