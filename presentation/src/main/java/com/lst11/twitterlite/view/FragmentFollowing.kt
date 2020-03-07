2fb2a5f119f466af08d6ddf2b3a980c00ec29bff
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-03-07 21:07:20 +0300

    created fragment following

---
+++ presentation/src/main/java/com/lst11/twitterlite/view/FragmentFollowing.kt
@@ -0,0 +1,21 @@
+package com.lst11.twitterlite.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lst11.twitterlite.R
+
+
+class FragmentFollowing : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_following, container, false)
+    }
+
+}
