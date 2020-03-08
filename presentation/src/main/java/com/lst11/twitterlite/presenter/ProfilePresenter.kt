8ffd1365275bc2840e58f83b928991b9164a2c0d
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-03-08 17:13:42 +0300

    profile presenter

---
+++ presentation/src/main/java/com/lst11/twitterlite/presenter/ProfilePresenter.kt
@@ -0,0 +1,67 @@
+package com.lst11.twitterlite.presenter
+
+import android.content.Context
+import com.google.firebase.database.FirebaseDatabase
+import com.lst11.twitterlite.R
+import com.lst11.twitterlite.UserService
+import javax.inject.Inject
+
+class ProfilePresenter @Inject constructor(private val context: Context) {
+
+    //TODO: move to dagger dependency
+    private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var userService: UserService
+
+    init {
+        userService = UserService(database)
+    }
+
+    fun buttonClicked(buttonName: String): List<String> {
+        when (buttonName) {
+            context.resources.getString(R.string.user_twits_button) -> {
+                return uploadUserPosts()
+            }
+
+            context.resources.getString(R.string.following_twits_button) -> {
+                return uploadFollowingPosts()
+            }
+            context.resources.getString(R.string.followers_twits_button) -> {
+                return uploadFollowersPosts()
+            }
+            else -> {
+                return emptyList()
+            }
+        }
+    }
+
+    fun uploadUserPosts(): MutableList<String> {
+        return listOf(
+            "User #1",
+            "User #2",
+            "User #3",
+            "User #4",
+            "User #5",
+            "User #6",
+            "User #7"
+        ).toMutableList()
+    }
+
+    private fun uploadFollowersPosts(): MutableList<String> {
+        return listOf(
+            "Follower #1",
+            "Follower #2",
+            "Follower #3",
+            "Follower #4"
+        ).toMutableList()
+    }
+
+    private fun uploadFollowingPosts(): MutableList<String> {
+        return listOf(
+            "Following #1",
+            "Following #2",
+            "Following #3",
+            "Following #4"
+        ).toMutableList()
+    }
+}
+
