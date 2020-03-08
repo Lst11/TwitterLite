3bd3da4ca76a9351a2765141ad76ac0e17d32be6
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-03-08 19:02:04 +0300

    added recycle view to following

--- presentation/src/main/java/com/lst11/twitterlite/view/fragment/FragmentFollowing.kt
+++ presentation/src/main/java/com/lst11/twitterlite/view/fragment/FragmentFollowing.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lst11.twitterlite.R
+import com.lst11.twitterlite.view.recyclerView.UserItemAdapter
 
 
 class FragmentFollowing : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +25,28 @@ class FragmentFollowing : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_following, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val following = listOf(
+            "Following #1",
+            "Following #2",
+            "Following #3",
+            "Following #4",
+            "Following #5"
+        )
+
+        viewManager = LinearLayoutManager(view.context)
+        viewAdapter = UserItemAdapter(following as MutableList<String>)
+
+        recyclerView = getView()!!.findViewById<RecyclerView>(R.id.posts).apply {
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
 
 }
