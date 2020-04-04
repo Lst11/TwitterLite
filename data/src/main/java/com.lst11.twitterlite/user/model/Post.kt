1042628f0e59c308ce79c645cd5d4d41a01b41d9
stanishevskaya.lena <stanishevskaya.lena@gmail.com>
2020-04-04 21:33:43 +0300

    modificators for the fields in the model were changed

--- data/src/main/java/com.lst11.twitterlite/user/model/Post.kt
+++ data/src/main/java/com.lst11.twitterlite/user/model/Post.kt
@@ -6,10 +6,22 @@ import java.io.Serializable
 @IgnoreExtraProperties
 class Post(
     var title: String = "",
-    var description: String = "",
-    var imageUrl: String = ""
+    var description: String = ""
 ) : Serializable {
+
+    private var imageUrl: String = ""
 
     private lateinit var authorId: String
 
+    fun setImageUrl(imageUrl: String) {
+        this.imageUrl = imageUrl
+    }
+
+    fun setAuthorId(imageUrl: String) {
+        this.authorId = authorId
+    }
+
+    fun getImageUrl(): String {
+        return imageUrl
+    }
 }
